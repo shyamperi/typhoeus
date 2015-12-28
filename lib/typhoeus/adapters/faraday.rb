0f2e7ef65e07651c070d07024d3acd4900ab2bad
Shyam Peri <shyam.peri@acm.org>
2015-12-28 19:41:29 +0530

    handle those cases when the host is unavailable

--- lib/typhoeus/adapters/faraday.rb
+++ lib/typhoeus/adapters/faraday.rb
@@ -90,6 +90,11 @@ module Faraday # :nodoc:
             unless parallel?(env)
               raise Faraday::Error::TimeoutError, "request timed out"
             end
+          elsif resp.return_code == :couldnt_resolve_host
+            env[:typhoeus_timed_out] = true
+            unless parallel?(env)
+              raise Faraday::Error::ConnectionFailed, resp.return_message
+            end
           end
 
           save_response(env, resp.code, resp.body) do |response_headers|
